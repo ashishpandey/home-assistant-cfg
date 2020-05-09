8b0d86a4ba1d0cef8dc1d1436ddbdad1770e970a
Ashish Pandey <ashish.pandey@gmail.com>
2020-05-09 20:15:43 +0800

    switched to code managed views

--- README.md
+++ README.md
@@ -7,8 +7,8 @@ My [Home Assistant](https://www.home-assistant.io/) Configuration
 _**Perennially** Work In Progress_
 
 > Needs a `secrets.yaml` to complete the configuration
+> Needs a `bin/.HA_TOKEN` to call HA APIs for development
 
-for emulated_hue, need to apply patch via deploy_patch.sh
+~for emulated_hue, need to apply patch via deploy_patch.sh~
  
 can work with this git repo cloned under /config
-
--- bin/ha_api.sh
+++ bin/ha_api.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
-if [ ! -f .HA_TOKEN ]; then
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+if [ ! -f ${DIR}/.HA_TOKEN ]; then
     echo "No HA API Token found"
     exit 1
 fi
 
 function call_api() {
   curl -X $1 \
-    -H "Authorization: Bearer $(cat .HA_TOKEN)" \
+    -H "Authorization: Bearer $(cat ${DIR}/.HA_TOKEN)" \
     -H "Content-Type: application/json" \
     http://hassio:8123/api/$2 2>/dev/null | json_pp
 }
